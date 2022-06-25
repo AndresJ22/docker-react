d0b8b0e14d02134f06515f53701cae37d7852762
AndresJ <joel.andres14@gmail.com>
2022-06-24 20:32:07 -0500

    version funcional con tiempo real yotube - no ude

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          as<code>src/App.js</code> and save to reload.sz
         </p>
+        <h1>hola andres 2022 !</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
